82fbcea0babc4ad96dab590d3d40b14d5aaf183b
quynh tran <quynhtranza2@gmail.com>
2021-11-15 14:34:55 +0700

    chinh confirm num status

--- app/Http/Controllers/TenantController.php
+++ app/Http/Controllers/TenantController.php
@@ -28,7 +28,7 @@ class TenantController extends Controller
             // 'tenant_user' =>$tenant_user,
             'tenant' => $tenant,
             'statusCode' => 1,
-            'tenantid' => $tenantId ,
+            'tenantid' => $tenantId,
         ]);
     }
     public function getNumRoom(Request $request)
@@ -46,31 +46,31 @@ class TenantController extends Controller
     }
     public function updateNumRoom(Request $request)
     {
+        $roomId = $request->roomId;
         $userId = $request->user()->id;
-        // $tenant = $this->spGetTenant($userId);
-        $tenant = $request->user()->latest_tenant_user->tenant;
-        $motel= $tenant->room->room_type->motel;
-        $room = $tenant->room;
-        $title = 'xác nhận phòng ' . $room->name .' Trọ '. $motel->name ;
-        $content = 'xác nhận số điện , nước ';
+        $room = Room::find($roomId);
+        $tenant = $room->latest_tenant;
+        $title = 'xác nhận phòng điện, nước ';
+        $content = 'xác nhận số điện , nước lúc vào trọ ';
         $sender_id = $userId;
-        $receiver_id = $room->room_type->motel->user->id;
+        $receiver = $room->latest_tenant->tenant_users;
         $noti_type_id = 4;
-        $tenant->num_status = 0;
-        $tenant->water_num = $request->water_num; // chua tesat
+        $tenant->water_num = $request->water_num;
         $tenant->elec_num = $request->elec_num;
         $tenant->save();
 
-        DB::transaction(function () use ($title, $sender_id, $receiver_id, $content, $noti_type_id) {
-            Noti::insert([
-                'title' => $title,
-                'sender_id' => $sender_id,
-                'receiver_id' => $receiver_id,
-                'content' => $content,
-                'noti_type_id' => $noti_type_id,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ]);
+        DB::transaction(function () use ($title, $sender_id, $receiver, $content, $noti_type_id) {
+            foreach ($receiver as $receiver_id) {
+                Noti::insert([
+                    'title' => $title,
+                    'sender_id' => $sender_id,
+                    'receiver_id' => $receiver_id->user_id,
+                    'content' => $content,
+                    'noti_type_id' => $noti_type_id,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ]);
+            }
         });
         return response()->json(['statusCode' => 1]);
     }
